394c59e9d1c2f9de9859d4ec6ce1f1ded436cc07
coldlarry <137831610@qq.com>
2020-12-17 13:49:05 +0800

    utils.py

--- torch_pruning/utils.py
+++ torch_pruning/utils.py
@@ -1,35 +1,38 @@
-from .dependency import TORCH_CONV ,TORCH_BATCHNORM ,TORCH_PRELU ,TORCH_LINEAR #line:1
-def count_prunable_params (O0O00O000OO0OO0O0 ):#line:3
-    if isinstance (O0O00O000OO0OO0O0 ,(TORCH_CONV ,TORCH_LINEAR )):#line:4
-        OOO0OOO0OO0OO0000 =O0O00O000OO0OO0O0 .weight .numel ()#line:5
-        if O0O00O000OO0OO0O0 .bias is not None :#line:6
-            OOO0OOO0OO0OO0000 +=O0O00O000OO0OO0O0 .bias .numel ()#line:7
-        return OOO0OOO0OO0OO0000 #line:8
-    elif isinstance (O0O00O000OO0OO0O0 ,TORCH_BATCHNORM ):#line:9
-        OOO0OOO0OO0OO0000 =O0O00O000OO0OO0O0 .running_mean .numel ()+O0O00O000OO0OO0O0 .running_var .numel ()#line:10
-        if O0O00O000OO0OO0O0 .affine :#line:11
-            OOO0OOO0OO0OO0000 +=O0O00O000OO0OO0O0 .weight .numel ()+O0O00O000OO0OO0O0 .bias .numel ()#line:12
-        return OOO0OOO0OO0OO0000 #line:13
-    elif isinstance (O0O00O000OO0OO0O0 ,TORCH_PRELU ):#line:14
-        if len (O0O00O000OO0OO0O0 .weight )==1 :#line:15
-            return 0 #line:16
-        else :#line:17
-            return O0O00O000OO0OO0O0 .weight .numel #line:18
-    else :#line:19
-        return 0 #line:20
-def count_prunable_channels (O0OO0O00OO0OOO000 ):#line:22
-    if isinstance (O0OO0O00OO0OOO000 ,TORCH_CONV ):#line:23
-        return O0OO0O00OO0OOO000 .weight .shape [0 ]#line:24
-    elif isinstance (O0OO0O00OO0OOO000 ,TORCH_LINEAR ):#line:25
-        return O0OO0O00OO0OOO000 .out_features #line:26
-    elif isinstance (O0OO0O00OO0OOO000 ,TORCH_BATCHNORM ):#line:27
-        return O0OO0O00OO0OOO000 .num_features #line:28
-    elif isinstance (O0OO0O00OO0OOO000 ,TORCH_PRELU ):#line:29
-        if len (O0OO0O00OO0OOO000 .weight )==1 :#line:30
-            return 0 #line:31
-        else :#line:32
-            return len (O0OO0O00OO0OOO000 .weight )#line:33
-    else :#line:34
-        return 0 #line:35
-def count_params (O00OO0OOO00OO000O ):#line:37
-    return sum ([O0O0O000OO0O00OOO .numel ()for O0O0O000OO0O00OOO in O00OO0OOO00OO000O .parameters ()])#line:38
+from .dependency import O0OOO00O0O0OOO0O0, O0OOOOOOO0O0O0OO0, O0OOO00O0O0O0O0O0, OO000OOOO000O0OOO
+
+def count_prunable_params(module):
+    if isinstance( module, ( O0OOO00O0O0OOO0O0, OO000OOOO000O0OOO) ):
+        num_params = module.weight.numel()
+        if module.bias is not None:
+            num_params += module.bias.numel()
+        return num_params
+    elif isinstance( module, O0OOOOOOO0O0O0OO0 ):
+        num_params = module.running_mean.numel() + module.running_var.numel()
+        if module.affine:
+            num_params+= module.weight.numel() + module.bias.numel()
+        return num_params
+    elif isinstance( module, O0OOO00O0O0O0O0O0 ):
+        if len( module.weight )==1:
+            return 0
+        else:
+            return module.weight.numel
+    else:
+        return 0
+
+def count_prunable_channels(module):
+    if isinstance( module, O0OOO00O0O0OOO0O0 ):
+        return module.weight.shape[0]
+    elif isinstance( module, OO000OOOO000O0OOO ):
+        return module.out_features
+    elif isinstance( module, O0OOOOOOO0O0O0OO0 ):
+        return module.num_features
+    elif isinstance( module, O0OOO00O0O0O0O0O0 ):
+        if len( module.weight )==1:
+            return 0
+        else:
+            return len(module.weight)
+    else:
+        return 0
+
+def count_params(module):
+    return sum([ p.numel() for p in module.parameters() ])
